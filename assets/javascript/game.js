d0c2f9ec16e5da670e6fd6e903818b00a084ac0e
jckent <chasekent30@gmail.com>
2019-09-02 15:38:31 -0400

    updated rng ranges and added win/loss logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,13 +4,29 @@ let wins= 0;
 let losses = 0;
 let crystalValues = [];
 
-// code for random array values, still needs to be limited to correct number range
-Array.from({length: 4}, () => Math.floor(Math.random() * ));
+// code for random array values,
+crystalValues = Array.from({length: 4}, () => Math.floor(Math.random() * (120 - 19 + 1)));
 
+//rng for goal
+
+Math.floor(Math.random() * (12 - 1 + 1)) + min;
 
 // adding button vals to counter
 $('.button').on('click', function() {
 let crystalValue= ($(this).attr('data-crystalvalue'));
 crystalValue=parseInt(crystalValue);
-counter+= crystalValue
+counter+= crystalValue;
+});
+
+//generic win/loss logic
+if (counter === goal) {
+    alert("You win!");
+    wins++;
+  }
+
+  else if (counter >= goal) {
+    alert("You lose!!");
+    losses++;
+  }
+
 });
