82aa5f22660878a5cda7cfd63b5b13f0e7f5d0a6
jckent <chasekent30@gmail.com>
2019-09-02 15:21:17 -0400

    onclick and array rng done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,14 @@ let counter = 0;
 let wins= 0;
 let losses = 0;
 let crystalValues = [];
+
+// code for random array values, still needs to be limited to correct number range
+Array.from({length: 4}, () => Math.floor(Math.random() * ));
+
+
+// adding button vals to counter
+$('.button').on('click', function() {
+let crystalValue= ($(this).attr('data-crystalvalue'));
+crystalValue=parseInt(crystalValue);
+counter+= crystalValue
+});
