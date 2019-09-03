b53e3ff74a30aaf7c61734135f8e7d56704562b3
jckent <chasekent30@gmail.com>
2019-09-02 20:24:09 -0400

    reworked js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,77 @@
+$( document ).ready(function(){
+//vars
 let goal;
 let counter = 0;
 let wins= 0;
 let losses = 0;
-let crystalValues = [];
+//random number for the goal value and appending to the right place in html
+goal = Math.floor(Math.random()*101+19);
 
-// code for random array values,
-crystalValues = Array.from({length: 4}, () => Math.floor(Math.random() * (120 - 19 + 1)));
+$('#goalNum').text(goal);
+//random numbers for each crystal
+let crystal1= Math.floor(Math.random()*11+1);
+let crystal2= Math.floor(Math.random()*11+1);
+let crystal3= Math.floor(Math.random()*11+1);
+let crystal4= Math.floor(Math.random()*11+1);
+//puts the counter in the right html spot
+$('#score').text(counter);
 
-//rng for goal
+//sets up onclick event for jewels
+$('#crystal1').on('click', function(){
+    counter = counter + crystal1;
+      $('#score').text(counter); 
+          //sets win/lose conditions
+        if (counter == goal){
+          youWin();
+        }
+        else if (counter > goal){
+          youLose();
+        }   
+  });  
+  $('#crystal2').on('click', function(){
+    counter = counter + crystal2;
+      $('#score').text(counter); 
+          //sets win/lose conditions
+        if (counter == goal){
+          youWin();
+        }
+        else if (counter > goal){
+          youLose();
+        }   
+  });  
+  $('#crystal3').on('click', function(){
+    counter = counter + crystal3;
+      $('#score').text(counter); 
+          //sets win/lose conditions
+        if (counter == goal){
+          youWin();
+        }
+        else if (counter > goal){
+          youLose();
+        }   
+  });
+  $('#crystal1').on('click', function(){
+    counter = counter + crystal1;
+      $('#score').text(counter); 
+          //sets win/lose conditions
+        if (counter == goal){
+          youWin();
+        }
+        else if (counter > goal){
+          youLose();
+        }   
+    });
 
-Math.floor(Math.random() * (12 - 1 + 1)) + min;
-
-// adding button vals to counter
-$('.button').on('click', function() {
-let crystalValue= ($(this).attr('data-crystalvalue'));
-crystalValue=parseInt(crystalValue);
-counter+= crystalValue;
-});
-
-//generic win/loss logic
-if (counter === goal) {
+//functions for win/loss
+function youWin () {
     alert("You win!");
     wins++;
+    $('#winner').text(wins);
   }
 
-  else if (counter >= goal) {
+  function youLose ()  {
     alert("You lose!!");
     losses++;
+    $('#loser').text(losses);
   }
-
 });
